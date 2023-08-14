a82203da2d09ec89abc94846ce23fe2e353c3f82
Alexey Skripalev <a.skripalev@gmail.com>
2023-08-14 10:35:39 +0300

    moving error handler to main controller

--- src/main/java/su/woland/learningspringboot/controller/PersonController.java
+++ src/main/java/su/woland/learningspringboot/controller/PersonController.java
@@ -1,16 +1,24 @@
 package su.woland.learningspringboot.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.*;
 import su.woland.learningspringboot.service.PersonService;
 import su.woland.learningspringboot.controller.dto.PersonRequestDto;
 import su.woland.learningspringboot.controller.dto.PersonResponseDto;
 
+import javax.servlet.http.HttpServletResponse;
+
 @AllArgsConstructor
 @RestController
-public class PersonController {
+public class PersonController implements ErrorController {
 
     private final PersonService personService;
+
+    @RequestMapping("/error")
+    public String error(HttpServletResponse response) {
+        return "Something went wrong: " + response.getStatus();
+    }
 
     @PostMapping("person")
     public PersonResponseDto save(@RequestBody PersonRequestDto personRequestDto) {
