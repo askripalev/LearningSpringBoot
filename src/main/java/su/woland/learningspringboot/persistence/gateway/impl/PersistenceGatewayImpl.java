056b52f540eb38177888c1fe0f2b171f97569d49
Alexey Skripalev <a.skripalev@gmail.com>
2023-08-15 18:33:42 +0300

    looks like change order of imports fixed some unknown bug

--- src/main/java/su/woland/learningspringboot/persistence/gateway/impl/PersistenceGatewayImpl.java
+++ src/main/java/su/woland/learningspringboot/persistence/gateway/impl/PersistenceGatewayImpl.java
@@ -2,9 +2,9 @@ package su.woland.learningspringboot.persistence.gateway.impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import su.woland.learningspringboot.persistence.PersonPersistence;
 import su.woland.learningspringboot.persistence.entity.PersonEntity;
 import su.woland.learningspringboot.persistence.gateway.PersistenceGateway;
+import su.woland.learningspringboot.persistence.PersonPersistence;
 
 @Component
 @AllArgsConstructor
