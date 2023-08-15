dd8e0834fcc72bac30ee62627c477ee13e3b29c1
Alexey Skripalev <a.skripalev@gmail.com>
2023-08-15 13:31:15 +0300

    move to springboot-3.1.2

--- src/main/java/su/woland/learningspringboot/controller/PersonController.java
+++ src/main/java/su/woland/learningspringboot/controller/PersonController.java
@@ -1,5 +1,6 @@
 package su.woland.learningspringboot.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.*;
@@ -7,8 +8,6 @@ import su.woland.learningspringboot.controller.mapper.RequestMapper;
 import su.woland.learningspringboot.service.PersonService;
 import su.woland.learningspringboot.controller.dto.PersonRequestDto;
 import su.woland.learningspringboot.controller.dto.PersonResponseDto;
-
-import javax.servlet.http.HttpServletResponse;
 
 @AllArgsConstructor
 @RestController
