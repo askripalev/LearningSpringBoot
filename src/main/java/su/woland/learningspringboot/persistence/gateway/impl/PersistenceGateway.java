0654ea140e7b5219279e3bb946c349319af24c88
Alexey Skripalev <a.skripalev@gmail.com>
2023-09-30 22:38:03 +0300

    fix naming

--- src/main/java/su/woland/learningspringboot/persistence/gateway/impl/PersistenceGateway.java
+++ src/main/java/su/woland/learningspringboot/persistence/gateway/impl/PersistenceGateway.java
@@ -1,17 +1,15 @@
 package su.woland.learningspringboot.persistence.gateway.impl;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import su.woland.learningspringboot.persistence.entity.Person;
 import su.woland.learningspringboot.persistence.repository.PersonRepository;
 
 @Component
 @AllArgsConstructor
-@Log4j2
 public class PersistenceGateway implements su.woland.learningspringboot.persistence.gateway.PersistenceGateway {
 
-    PersonRepository personRepository;
+    private final PersonRepository personRepository;
 
     @Override
     public Person savePerson(Person person) {
